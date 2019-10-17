182298a415ef1e4e7f4d21f9e7b4ed759907a659
Thomas Emery <tkemery7@gmail.com>
2019-10-16 23:34:45 -0400

    app now removes previous images

--- index.js
+++ index.js
@@ -1,29 +1,34 @@
 function handleSubmit(){
-    $('form').submit(event => {
-      event.preventDefault();
-      let number = document.getElementById('number').value;
-      getDogPics(number);
-    });
-  }
+  $('form').submit(event => {
+    event.preventDefault();
+    let number = document.getElementById('number').value;
+    getDogPics(number);
+  });
+}
   
 function getDogPics(number){
-    number = number || 3;
-    fetch(`https://dog.ceo/api/breeds/image/random/${number}`)
-    .then(response => response.json())
-    .then(function(responseJson){
-        displayResults(responseJson);
-        for (let i=0; i < responseJson.message.length; i++){
-            console.log(responseJson.message[i]);
-        }
-    }).catch(error => alert('Something went wrong. Try again later.'));
+  number = number || 3;
+  fetch(`https://dog.ceo/api/breeds/image/random/${number}`)
+  .then(response => response.json())
+  .then(function(responseJson){
+      displayResults(responseJson);
+      for (let i=0; i < responseJson.message.length; i++){
+        console.log(responseJson.message[i]);
+      }
+  }).catch(error => alert('Something went wrong. Try again later.'));
 }
   
 function displayResults(responseJson){
+  let currentDoggos = $('.container').children();
+  currentDoggos.remove();
   $('.container').removeClass('hidden')
   responseJson.message.forEach(function(item){
       $('.container').append(`<img src='${item}' width='300px'>`)
   });
 }
+// function displayResults(responseJson){
+//     $('img').attr('src', `${responseJson.message}`);
+// }
   
 $(function(){
   handleSubmit();
